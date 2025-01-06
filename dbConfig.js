a6c4257dd6eafbf3395c74419fb09bccb60e617a
almadendj <almadendj@gmail.com>
2025-01-06 14:29:25 +0800

    revert previous dbconfig fix

--- dbConfig.js
+++ dbConfig.js
@@ -1,24 +1,20 @@
 const mysql = require('mysql');
 
-const createDBConnection = () => {
-  const dbConfig = {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "sethnl99",
-    database: process.env.DB_NAME || "errandcatcher",
-  };
-
-  const connection = mysql.createConnection(dbConfig);
-
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      return;
-    }
-    console.log("Connected to MySQL database");
-  });
-
-  return connection;
+const dbConfig = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "sethnl99",
+  database: process.env.DB_NAME || "errandcatcher",
 };
 
-module.exports = createDBConnection;
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+module.exports = connection;
